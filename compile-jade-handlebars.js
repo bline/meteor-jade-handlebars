1c6bc1b39d615a76a8734571cd9719cc4eda98db
Scott Beck <scottbeck@gmail.com>
2013-09-11 16:38:37 +0400

    Must of the StringScanner code is unchanged from the original, however for some reason I had to fix some of the regular expressions used in trying to find beginning space, basically it was using /(\ *)(.*)\n+/ which failed in case of tabs. It also fails on the last line if there is no new line. It also now checks the return of scan now so it doesn't just die on errors. Currently it uses console.log to log the file it is failing on now, but this should probably throw an exception and give the line number the issue is on. Will revisit and clean this up later but for now it is working for me.

---
+++ compile-jade-handlebars.js
@@ -0,0 +1,182 @@
+
+StringScanner = Npm.require('StringScanner');
+jade = Npm.require('jade');
+path = Npm.require('path');
+
+var handler = function(compileStep) {
+  if (! compileStep.archMatches('browser'))
+    return;
+
+  // Variables
+  var lines = [];
+  var json = [];
+  var handlebarsPattern = /\s*(\{\{.*(?!\{\{)\}\})/;
+
+  // Handlebars hack
+  // Read the file content and create JSON
+  try{
+    // Create the string scanner with the .jade file content
+    var ss = new StringScanner(compileStep.read().toString('utf8'));
+    ss.reset();
+    // Parse the file content until the end
+    while(!ss.endOfString()){
+      // Scan content per line
+      var res = ss.scan(/^(\s*)(.*)\n*/);
+      if (typeof res === "undefined" || res === null) {
+        console.log("failed to scan!", compileStep.inputPath);
+        break
+      }
+
+      // Get the indentation of the line
+      var captures = ss.captures() || [];
+      var first = captures[0] || '';
+      indent = first.length;
+      // Get the content of the line
+      value = captures[1] || '';
+
+      // Variables for json
+      var child = [];
+      var tags = []
+
+      // Scan the content of the line to find handlebars tags
+      ss_line = new StringScanner(value);  
+      ss_line.reset();
+      while(ss_line.checkUntil(handlebarsPattern)){
+        captures = ss_line.captures() || [];
+        first = captures[0] || '';
+        ss_line.scanUntil(handlebarsPattern);
+        tags.push({"position": ss_line.pointer()-first.length, "value": first});
+      }
+      // End scan
+      ss_line.terminate();
+
+      // Create the JSON for the line
+      line = {"indent": indent, "content": value, "tags": tags, "child": child};
+
+      // Find arborescence
+      // If the line is root
+      if(line.indent == 0){
+        // Add to the main JSON
+        json.push(line);
+      }else{
+        // Add the child to the parent
+        for(var i=lines.length-1; i >= 0; i--){
+          if(lines[i].indent < line.indent){
+            lines[i].child.push(line);
+            break;
+          }
+        }  
+      }  
+      lines.push(line);   
+    }
+    // End scan
+    ss.terminate();
+  } catch(err) {
+    // XXX better error handling, once the Plugin interface support it
+    throw err
+    console.log("Error: ", err.toSource());
+    throw new Error(
+      compileStep.inputPath + ':' +
+      err.message
+    );
+
+  }
+
+  // Fix indentation
+  json = jsonParser(json);
+
+  // JSON to string
+  global.contents_tmp = ""; // used in jsonToContents() function
+  contents = jsonToContents(json); 
+  
+  // Jade parser
+  var jade_options = { pretty: true };
+
+  jade.render(contents, jade_options, function(err, html){
+    if (err) throw err;
+    contents = html;
+  });
+
+
+  // from meteor compile-template.js
+  try {
+    var results = html_scanner.scan(contents.toString('utf8'), compileStep.inputPath);
+  } catch (e) {
+    if (e instanceof html_scanner.ParseError) {
+      compileStep.error({
+        message: e.message,
+        sourcePath: compileStep.inputPath,
+        line: e.line
+      });
+      return;
+    } else
+      throw e;
+  }
+
+  if (results.head)
+    compileStep.appendDocument({ section: "head", data: results.head });
+
+  if (results.body)
+    compileStep.appendDocument({ section: "body", data: results.body });
+
+  if (results.js) {
+    var path_part = path.dirname(compileStep.inputPath);
+    if (path_part === '.')
+      path_part = '';
+    if (path_part.length && path_part !== path.sep)
+      path_part = path_part + path.sep;
+    var ext = path.extname(compileStep.inputPath);
+    var basename = path.basename(compileStep.inputPath, ext);
+
+    // XXX generate a source map
+
+    compileStep.addJavaScript({
+      path: path.join(path_part, "template." + basename + ".js"),
+      sourcePath: compileStep.inputPath,
+      data: results.js
+    });
+  }
+}
+
+function jsonParser(json) {
+  // Number fo indentation
+  n = 2;
+  // Start the loop
+  json.forEach(function(root){
+    root.child.forEach(function(child){            
+      // If line doesn't have HB tag
+      if(root.tags.length < 1){
+        child.indent = root.indent+n;
+      }
+      // If line has HB tag and start with HB tag and not comment
+      else if(root.tags.length > 0 && root.tags[0].position == 0 && !root.content.match(/^\/\/\-*.*/)) {
+          child.indent = root.indent;
+      }
+      else if(root.tags.length > 0 && root.tags[0].position != 0 && !root.content.match(/^\/\/\-*.*/)) {
+          child.indent = root.indent+n;
+      }
+      // If child has child, recursive call  
+      if(child.child.length > 0)
+        jsonParser([child]);
+    });
+  });
+  return json;
+}
+
+function jsonToContents(json) {
+  for(line in json){
+    for(attr in json[line]){
+        
+      if(attr == "content"){
+        global.contents_tmp += Array(json[line].indent+1).join(" ") + json[line][attr] + "\n";
+      } 
+      else if(attr == "child" && typeof(json[line][attr]) == "object") {
+          jsonToContents(json[line][attr]);
+      }
+    }
+  }
+  return global.contents_tmp;
+}
+
+Plugin.registerSourceHandler("jade", handler);
+
